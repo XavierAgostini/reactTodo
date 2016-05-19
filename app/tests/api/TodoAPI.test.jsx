b0bd7eb0a453023623737c5f79e5a3a1d40e06d8
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2016-05-19 17:43:24 -0400

    add todo filtering

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -52,4 +52,30 @@ describe('TodoAPI', () => {
 			expect(actualTodos).toEqual(todos);
 		})
 	});
+
+	describe('filterTodos', () => {
+		var todos = [{
+			id: 1,
+			text: 'a',
+			completed: true
+		}, {
+			id: 2,
+			text: 'b',
+			completed: false
+		}, {
+			id: 3,
+			text: 'c',
+			completed: true
+		}];
+
+		it('Should return all items if showCompleted is true', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filteredTodos.length).toBe(3);
+		});
+
+		it('Should not show completed todos if showCompleted is true', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+			expect(filteredTodos.length).toBe(1);
+		})
+	});
 });
